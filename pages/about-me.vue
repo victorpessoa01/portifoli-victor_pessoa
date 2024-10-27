6a8430cfe2b1cefb121104f57f8693d367426a28
victorpessoa01 <victorpessoa0011@gmail.com>
2024-10-27 14:01:44 -0300

    chore: tudo certo

--- pages/about-me.vue
+++ pages/about-me.vue
@@ -1,26 +1,6 @@
 <template>
     <v-container class="container">
-        <v-row justify="center">
-            <v-col cols="12" md="8">
-                <h1 class="text-h3 font-weight-bold">Sobre Mim</h1>
-                <p class="text-body-1">
-                    Olá! Meu nome é Victor e sou um desenvolvedor backend com experiência em projetos 
-                    voltados para a comunidade. Tenho um grande interesse em tecnologias como JavaScript, 
-                    TypeScript, Vue.js, Nuxt.js e PostgreSQL.
-                </p>
-                <h2 class="text-h5 font-weight-bold">Missão</h2>
-                <p class="text-body-1">
-                    Meu objetivo é desenvolver aplicações que ajudem a comunidade, especialmente na área 
-                    religiosa. Acredito que a tecnologia pode ser uma ferramenta poderosa para a transformação social.
-                </p>
-                <h2 class="text-h5 font-weight-bold">Interesses</h2>
-                <ul>
-                    <li>Desenvolvimento Web</li>
-                    <li>Inteligência Artificial</li>
-                    <li>Projetos Sociais</li>
-                </ul>
-            </v-col>
-        </v-row>
+    
     </v-container>
 </template>
 
--- pages/home.vue
+++ pages/home.vue
@@ -1,15 +1,6 @@
 <template>
     <v-container class="container">
-        <v-row justify="center">
-            <v-col cols="12" md="8">
-                <h1 class="text-h3 font-weight-bold">Bem-vindo ao meu Portfólio!</h1>
-                <p class="text-body-1">
-                    Sou Victor, um desenvolvedor de software apaixonado por criar soluções inovadoras. 
-                    Neste portfólio, você encontrará informações sobre mim, minhas habilidades e projetos.
-                </p>
-                <v-btn color="primary" @click="$router.push('/about')">Saiba Mais Sobre Mim</v-btn>
-            </v-col>
-        </v-row>
+       
     </v-container>
 </template>
 
