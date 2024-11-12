424e66025a6c23ee1b136dd76ed12999a57b4e3f
victorpessoa01 <victorpessoa0011@gmail.com>
2024-11-12 08:58:10 -0300

    vercel

--- .nuxt/types/plugins.d.ts
+++ .nuxt/types/plugins.d.ts
@@ -16,6 +16,7 @@ type NuxtAppInjections =
   InjectionType<typeof import("../../node_modules/.pnpm/nuxt@3.13.2_@parcel+watcher@2.4.1_@types+node@22.8.1_ioredis@5.4.1_magicast@0.3.5_rollup@4.24_ouildywjvh6pvsm3oim64rmrwe/node_modules/nuxt/dist/app/plugins/revive-payload.server.js").default> &
   InjectionType<typeof import("../../node_modules/.pnpm/nuxt@3.13.2_@parcel+watcher@2.4.1_@types+node@22.8.1_ioredis@5.4.1_magicast@0.3.5_rollup@4.24_ouildywjvh6pvsm3oim64rmrwe/node_modules/nuxt/dist/app/plugins/chunk-reload.client.js").default> &
   InjectionType<typeof import("../../node_modules/.pnpm/nuxt@3.13.2_@parcel+watcher@2.4.1_@types+node@22.8.1_ioredis@5.4.1_magicast@0.3.5_rollup@4.24_ouildywjvh6pvsm3oim64rmrwe/node_modules/nuxt/dist/pages/runtime/plugins/prefetch.client.js").default> &
+  InjectionType<typeof import("../../plugins/vercel-analytics.client").default> &
   InjectionType<typeof import("../../plugins/vuetify").default>
 
 declare module '#app' {
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,6 @@ export default defineNuxtConfig({
   build: {
     transpile: ['vuetify'],
   },
-  plugins: ['~/plugins/vercel-analytics.ts'],
+  plugins: ['~/plugins/vercel-analytics.client.ts'],
   compatibilityDate: '2024-10-18', 
 });
